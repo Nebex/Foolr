a69d80cbc4ff0608312242f10b4b22c49a765ac2
Nebex <struhlboy@gmail.com>
2015-08-04 14:27:03 -0400

    load button
    added load button and on click

--- app/src/main/java/com/example/user/foolr/SelectPhoto.java
+++ app/src/main/java/com/example/user/foolr/SelectPhoto.java
@@ -20,8 +20,14 @@ public class SelectPhoto extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_photo);
+        ImageView pic = (ImageView) findViewById(R.id.imgView);
     }
-
+    pic.setOnClickListener(new Buttton.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(intent);
+        }
+    });
     public void loadImagefromGallery(View view) {
         // Create intent to Open Image applications like Gallery, Google Photos
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
